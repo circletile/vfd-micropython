deaae24c7922029edb3f5d9bde415e36f071f3bf
circletile <circletile@users.noreply.github.com>
2022-08-22 17:59:25 -0400

    Create main and clock module files

---
+++ main.py
@@ -0,0 +1,10 @@
+#
+# MicroPython Firmware for David Pye VFD Clock
+#
+# Main Module
+#
+# Chris Upchurch (cpuburner@gmail.com)
+#
+
+import vfd_pyclock
+vfd_pyclock.main()
---
+++ vfd_pyclock.py
@@ -0,0 +1,8 @@
+#
+# MicroPython Firmware for David Pye VFD Clock
+#
+# Clock Core Module
+#
+# Chris Upchurch (cpuburner@gmail.com)
+#
+
